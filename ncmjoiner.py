5cab94d5c5ad323596075d9a83d18cf14feb902e
@hashtagcyber <matthew.domko@gmail.com>
2015-12-01 12:01:55 -0500

    Added argv... need to learn how to print usage... ./ncmjoiner.py '/path/to/files' 'EndOfFilename'

---
+++ ncmjoiner.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+from sys import argv
+import glob
+targetdir=argv[1]
+endswith=argv[2]
+
+#Create an Array listing all files in the target directory
+almosthost=glob.glob(targetdir+"*"+endswith)
+hostlist=[]
+# Strip the 
+for i in almosthost:
+	hostlist.append(i.rstrip("showrun"))
+for host in hostlist:
+	hostfiles=[]
+	hostfiles.append(host+"showversion")
+	hostfiles.append(host+"showrun")
+	hostfiles.append(host+"showcdpneighboor")
+	with open(host + "results.txt",'w') as outfile:
+		for fname in hostfiles:
+			with open(fname) as infile:
+				outfile.write(infile.read())
